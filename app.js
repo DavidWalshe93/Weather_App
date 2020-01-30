d20d0a66d2e8c850a426f929a38893a844aa0ced
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-01-30 22:49:53 +0000

    Added Geo-forwarding end-point and request to endpoint to obtain lat,long for a given address

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const request = require("request");
 
-const url = process.env.DARKSKY;
+// Weather
+const DARKSKY_TOKEN = process.env.DARKSKY_TOKEN;
 const lat = 37.8267;
 const long = -122.4233;
-const endpoint = `${url}${lat},${long}?units=si`;
+const darksky_endpoint = `https://api.darksky.net/forecast/${DARKSKY_TOKEN}/${lat},${long}?units=si`;
+
+console.log(darksky_endpoint);
 
 
-request({url: endpoint, json: true }, (error, response) => {
+request({url: darksky_endpoint, json: true }, (error, response) => {
     const day_summary = response.body.daily.data[0].summary;
     const current = response.body.currently;
     console.log(`${day_summary} It is currenlty ${current.temperature} degrees out. There is a ${current.humidity}% chance of rain.`);
+});
+
+// Forward Geocoding
+const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
+const address = "Los Angeles".replace(" ", "%20");
+const map_box_endpoint = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${MAPBOX_TOKEN}&limit=1`;
+
+console.log(map_box_endpoint);
+
+request({url: map_box_endpoint, json: true}, (error, response) => {
+    const latitude = response.body.features[0].center[1];
+    const longtitude = response.body.features[0].center[0];
+    console.log(`Lat: ${latitude}, Long: ${longtitude}`)
 });
