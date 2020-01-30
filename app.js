2d9f357faca64286644fc4ecd613b084cb05b401
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-01-30 22:19:09 +0000

    Parsed JSON response of HTTP request.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const request = require("request");
 
 const url = process.env.DARKSKY;
-const lat_long = "37.8267,-122.4233";
-const endpoint = url + lat_long;
+const lat = 37.8267;
+const long = -122.4233;
+const endpoint = `${url}${lat},${long}?units=si`;
 
 
 request({url: endpoint, json: true }, (error, response) => {
-    console.log(response.body.currently);
+    const day_summary = response.body.daily.data[0].summary;
+    const current = response.body.currently;
+    console.log(`${day_summary} It is currenlty ${current.temperature} degrees out. There is a ${current.humidity}% chance of rain.`);
 });
