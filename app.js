00cbaccd44d3a9712d4642e4d344a7bd832649f5
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-01-30 23:35:47 +0000

    Chained geocoding and weather forecasting calls to give a weather forecast for a input address

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 const request = require("request");
 
 const geocode = require("./geocode");
-
-// Weather
-const DARKSKY_TOKEN = process.env.DARKSKY_TOKEN;
-const lat = 37.8267;
-const long = -122.4233;
-const darksky_endpoint = `https://api.darksky.net/forecast/${DARKSKY_TOKEN}/${lat},${long}?units=si`;
-
-console.log(darksky_endpoint);
-
-
-request({url: darksky_endpoint, json: true }, (error, response) => {
-    if (error) {
-        console.log("Unable to connect to weather forecast service")
-    } else if (response.body.error) {
-        console.log("Unable to find location")
-    } else {
-        const day_summary = response.body.daily.data[0].summary;
-        const current = response.body.currently;
-        console.log(`${day_summary} It is currenlty ${current.temperature} degrees out. There is a ${current.humidity}% chance of rain.`);
-    }
-});
+const forecast = require("./forecast");
 
 geocode("Dublin", (error, data) => {
-    console.log("Error", error);
-    console.log("Data", data);
+    if (error) {
+        console.log("Error", error);
+    } else {
+        forecast(data.latitude, data.longitude, (error, data) => {
+            if(error) {
+                console.log("Error", error)
+            } else {
+                console.log(data)
+            }
+        })
+    }
 });
---
+++ forecast.js
@@ -0,0 +1,23 @@
+const request = require("request");
+
+
+const forecast = (latitude, longitude, callback) => {
+    const DARKSKY_TOKEN = process.env.DARKSKY_TOKEN;
+    const darksky_endpoint = `https://api.darksky.net/forecast/${DARKSKY_TOKEN}/${latitude},${longitude}?units=si`;
+
+    request({url: darksky_endpoint, json: true }, (error, response) => {
+        if (error) {
+            callback("Unable to connect to weather forecast service", undefined)
+        } else if (response.body.error) {
+            callback("Unable to find location", undefined);
+        } else {
+            const day_summary = response.body.daily.data[0].summary;
+            const current = response.body.currently;
+            callback(undefined, `${day_summary} It is currenlty ${current.temperature} degrees out. There is a ${current.humidity}% chance of rain.`);
+        }
+    })
+};
+
+
+module.exports = forecast;
+
