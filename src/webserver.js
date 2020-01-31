dedbdd9b78a007656c8e272d9086e79b0c3b2877
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-01-31 20:27:13 +0000

    Used handlebars (hbs) for express to show dynamic server content in browser views

--- src/webserver.js
+++ src/webserver.js
@@ -4,7 +4,30 @@ const express = require('express');
 
 const app = express();
 
+app.set("view engine", "hbs");
+
 app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("", (req, res) => {
+    res.render("index", {
+        title: "Weather App",
+        name: "Dave"
+    })
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About Me",
+        name: "Dave"
+    })
+});
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        title: "Help",
+        message: "This is a page to show help messages"
+    })
+});
 
 app.get("/weather", (req, res) => {
     res.send("Weather page")
