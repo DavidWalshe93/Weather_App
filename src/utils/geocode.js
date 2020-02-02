a79839c8cd595e0d5048596a08e4903a7d686434
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-02 18:21:54 +0000

    Fixed an issue with feature retrieval crashing code.

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -5,10 +5,13 @@ const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${MAPBOX_TOKEN}&limit=1`;
 
     request({url, json: true}, (error, {body}) => {
+
         if (error) {
             callback("Unable to connect to geo-coding service", undefined)
-        } else if (body.features.length === 0) {
-            callback("Address does not match any coordinates", undefined)
+        } else if (body.features) {
+            if (body.features.length === 0) {
+                callback("Address does not match any coordinates", undefined)
+            }
         } else {
             callback(undefined, {
                 latitude: body.features[0].center[1],
