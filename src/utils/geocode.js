fd5c097c3cac2f65fa53a83f59fcd1bc9026b4e8
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-02 20:06:15 +0000

    Testing

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,8 +2,8 @@ const request = require("request");
 
 const geocode = (address, callback) => {
     const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
-    // const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${MAPBOX_TOKEN}&limit=1`;
-    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/New York.json?access_token=pk.eyJ1IjoiZHdhbHNoZTkzIiwiYSI6ImNrNjFheXE5dTAxZTcza28wZHhqNWtlZGkifQ.Tan1qtJ79NtS4qK2xgpHSQ&limit=1";
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${MAPBOX_TOKEN}&limit=1`;
+    // const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/New York.json?access_token=pk.eyJ1IjoiZHdhbHNoZTkzIiwiYSI6ImNrNjFheXE5dTAxZTcza28wZHhqNWtlZGkifQ.Tan1qtJ79NtS4qK2xgpHSQ&limit=1";
     console.log(url)
     request({url, json: true}, (error, {body}) => {
         if (error) {
