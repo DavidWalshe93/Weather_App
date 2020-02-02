6efc3f14d8893baba3487e8285fb01b9cf5c542a
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-02 16:26:22 +0000

    Added a Javascript promise to fetch data from /weather endpoint

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 console.log("Client side javascript file loaded");
+
+fetch("http://localhost:3000/weather").then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data);
+        }
+    })
+});
