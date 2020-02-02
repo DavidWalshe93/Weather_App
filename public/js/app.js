df15af3b8a00829105dd3b748fb673bceca7d89b
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-02 17:59:50 +0000

    Added code changes to allow for app to be deployed on Heroku.

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ weatherForm.addEventListener("submit", (e) => {
     temperatureMsg.textContent = "";
     humidityMsg.textContent = "";
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 locationMsg.textContent = "Error: " + data.error;
