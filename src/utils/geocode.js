87ed03a257614564255efa5c77a0cb927f436fd7
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-02 20:02:39 +0000

    Testing

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,17 +2,16 @@ const request = require("request");
 
 const geocode = (address, callback) => {
     const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${MAPBOX_TOKEN}&limit=1`;
+    // const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${MAPBOX_TOKEN}&limit=1`;
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/shannon.json?access_token=pk.eyJ1IjoiZHdhbHNoZTkzIiwiYSI6ImNrNjFheXE5dTAxZTcza28wZHhqNWtlZGkifQ.Tan1qtJ79NtS4qK2xgpHSQ&limit=1";
     console.log(url)
     request({url, json: true}, (error, {body}) => {
         if (error) {
             console.error("Unable to connect to geo-coding service");
             callback("Unable to connect to geo-coding service", undefined)
-        } else if (body.features) {
-            if (body.features.length === 0) {
-                console.log("Address does not match any coordinates");
-                callback("Address does not match any coordinates", undefined)
-            }
+        } else if (body.features.lenght === 0) {
+            console.log("Address does not match any coordinates");
+            callback("Address does not match any coordinates", undefined)
         } else {
             console.log(body);
             callback(undefined, {
